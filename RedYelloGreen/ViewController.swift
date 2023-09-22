61e766924e693194b6bd9f217a25d704d54fab69
Evgeny Likhachev <EvgenyLi429@gmail.com>
2023-09-22 15:50:09 +0200

    Initial Commit

--- RedYelloGreen/ViewController.swift
+++ RedYelloGreen/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped() {
+    }
+    
 }
 
