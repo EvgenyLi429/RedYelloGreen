2fd4d14aac171236a758f3a2b000ba0b202cf30f
Evgeny Likhachev <EvgenyLi429@gmail.com>
2023-09-24 11:22:13 +0200

    Interface changes. Still no logic integrated

--- RedYelloGreen/ViewController.swift
+++ RedYelloGreen/ViewController.swift
@@ -16,9 +16,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.alpha = 0.1
+        yellowLight.alpha = 0.1
+        greenLight.alpha = 0.1
+        actionButton.layer.cornerRadius = 15
+        redLight.layer.cornerRadius = 64
+        yellowLight.layer.cornerRadius = 64
+        greenLight.layer.cornerRadius = 64
     }
 
     @IBAction func buttonTapped() {
+        actionButton.setTitle("NEXT", for: .normal)
+        
+        /*if no Lights aplha 1 switch red light
+         switch case:
+         a che esli sdelat 000 = vrubai 100
+         100 vrubai 010
+         010 vrubai
+         
+        */
+        
     }
     
 }
